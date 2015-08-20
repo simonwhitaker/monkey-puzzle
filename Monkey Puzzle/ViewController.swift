a905f35bc08a77c633a3eac7561cf7daa862c622
Simon Whitaker <sw@netcetera.org>
2015-08-20 19:53:00 +0100

    Initial Commit

---
+++ Monkey Puzzle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Monkey Puzzle
+//
+//  Created by Simon on 20/08/2015.
+//  Copyright © 2015 Simon Whitaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
